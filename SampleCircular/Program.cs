c6d97146cf6aaec5bc3a4e74aa7221d2893ed4d7
Hamam Priyatmoko <hamampriyatmoko81@gmail.com>
2022-12-15 16:28:13 +0700

    Menambahkan array kedalam method

--- SampleCircular/Program.cs
+++ SampleCircular/Program.cs
@@ -12,7 +12,9 @@ namespace SampleCircular
         int[] queue_array = new int[5];
         public Queues()
         {
-
+            /*Initializing the values of the variabels REAR and FRONT to -1 to indicate that the queue is initially empty. */
+            FRONT = -1;
+            REAR = -1;
         }
         static void Main(string[] args)
         {
