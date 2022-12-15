383f66e454115a5b42a9211d5038a60b46a9c616
Hamam Priyatmoko <hamampriyatmoko81@gmail.com>
2022-12-15 16:23:37 +0700

    menambahkan parameter

--- SampleCircular/Program.cs
+++ SampleCircular/Program.cs
@@ -8,6 +8,8 @@ namespace SampleCircular
 {
     class Queues
     {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
         static void Main(string[] args)
         {
         }
