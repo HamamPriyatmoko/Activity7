8cce20fef0629e70c78be18a008e8973b3fa8103
Hamam Priyatmoko <hamampriyatmoko81@gmail.com>
2022-12-15 16:25:11 +0700

    membuat method public Queues

--- SampleCircular/Program.cs
+++ SampleCircular/Program.cs
@@ -10,6 +10,10 @@ namespace SampleCircular
     {
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
+        public Queues()
+        {
+
+        }
         static void Main(string[] args)
         {
         }
